a073e6405066096fcda7f4b74ca8a5c185a322ee
Nik Nyby <nnyby@columbia.edu>
2017-02-22 10:34:22 -0500

    Load example JSON data in ctlevent tests

--- tests/test-ctlevent.js
+++ tests/test-ctlevent.js
@@ -1,17 +1,19 @@
 var assert = require('assert');
-var CTLEvent = require('../ctlevent.js');
+var CTLEvent = require('../ctlevent.js').CTLEvent;
+var fs = require('fs');
 
 describe('CTLEvent', function() {
+    var json = JSON.parse(fs.readFileSync('./tests/data.json', 'utf8'));
+    var events = json.bwEventList.events;
+
     describe('constructor', function() {
-        console.log(CTLEvent.CTLEvent);
         it('should not fail when given empty data', function() {
-            var e = CTLEvent.CTLEvent({});
+            var e = new CTLEvent({});
         });
         it('loads data correctly', function() {
-            var json = {};
-            var e = CTLEvent.CTLEvent(json);
-            assert.equal(e.id, json['id']);
-            assert.equal(e.start, json['id']);
+            var e = new CTLEvent(events[0]);
+            assert.equal(e.id, events[0].guid);
+            assert.equal(e.start, events[0].start_longdate);
         });
     });
 });
